322d3b57c64ec6db7547ebad9a3dde07dee30734
Jessada <87136429+johnisu2@users.noreply.github.com>
2025-12-29 17:36:57 +0700

    Update route.ts

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -7,6 +7,8 @@ export async function POST(request: NextRequest) {
     try {
         const { documentId, json } = await request.json();
 
+        const fullData: any = {}
+
         console.log(`[API Generate] Request for documentId: ${documentId}`);
         console.log(`[API Generate] JSON Context:`, JSON.stringify(json, null, 2));
 
@@ -18,6 +20,53 @@ export async function POST(request: NextRequest) {
             where: { id: parseInt(documentId) },
             include: { elements: true }
         });
+
+        console.log("doc :", doc);
+
+        if (doc?.elements && doc.elements.length > 0) {
+            const tableIds = Array.from(
+                new Set(
+                    doc.elements
+                        .map(el => el.dbConfigTableId)
+                        .filter((id): id is number => typeof id === "number")
+                )
+            );
+
+            console.log("tableIds : ", tableIds); // [1, 2]
+
+            const findProcedure = await prisma.dbConfigTable.findMany({
+                where: { id: { in: tableIds } },
+                include: {
+                    params: {
+                        orderBy: {
+                            seq: "asc", // หรือ "desc"
+                        },
+                    },
+                },
+            })
+
+            findProcedure.forEach(async (table) => {
+                if (table.procedureName) {
+                    try {
+                        const params: any = [];
+                        table.params.forEach((param: any) => {
+                            params.push(json[param.paramName] || null);
+                        });
+
+                        console.log("params : ", params);
+
+                        const data: any = await prisma.$queryRawUnsafe(`SELECT * FROM ${table.procedureName}(${params?.map((p: any, index: number) => `$${index + 1}`).join(',')});`, ...params);
+                        fullData[table.tableName] = data[0];
+
+                        console.log("fullData : ", fullData)
+                    } catch (procError) {
+                        console.error(`Error executing procedure ${table.procedureName}:`, procError);
+                    }
+                }
+            })
+        }
+
+        // return NextResponse.json({ msg: "DONE PROCESS" }, { status: 200 });
 
         if (doc) {
             console.log(`[API Generate] Found Document: ID=${doc.id}, Type=${doc.fileType}, URL=${doc.fileUrl}`);
@@ -49,7 +98,7 @@ export async function POST(request: NextRequest) {
             metadata: el.metadata || undefined
         }));
 
-        const pdfBytes = await generatePdf(doc.fileUrl, elements, json || {}, doc.fileType);
+        const pdfBytes = await generatePdf(doc.fileUrl, elements, fullData || {}, doc.fileType);
 
         // Convert Uint8Array to Buffer for Next.js response compatibility
         return new NextResponse(Buffer.from(pdfBytes), {
